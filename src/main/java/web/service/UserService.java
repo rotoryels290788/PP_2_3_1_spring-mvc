8a4aab277532c1d71f871033266caba1cdabad4b
Dmitry Kharin <104873585+rotoryels290788@users.noreply.github.com>
2022-12-17 13:29:48 +0300

    Update UserService.java

--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,9 +1,7 @@
 package web.service;
-import org.springframework.stereotype.Component;
 import web.model.User;
 import java.util.List;
 
-@Component
 public interface UserService {
 
     List<User> getUserAll();
