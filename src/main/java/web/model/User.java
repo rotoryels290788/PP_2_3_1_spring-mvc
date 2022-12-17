e9216eda3b6e996a5bb3e2ca40cf1142019f4a70
Dmitry Kharin <104873585+rotoryels290788@users.noreply.github.com>
2022-12-17 14:15:40 +0300

    Update User.java

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,9 +1,10 @@
 package web.model;
 
-
 import javax.persistence.*;
 import java.util.Objects;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -18,68 +19,4 @@ public class User {
     private String userName;
     @Column(name = "email")
     private String email;
-
-    public User() {
-    }
-
-    public User(String name, String userName, String email) {
-        this.name = name;
-        this.userName = userName;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id.equals(user.id) && name.equals(user.name) && userName.equals(user.userName) && email.equals(user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, userName, email);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", userName='" + userName + '\'' +
-                ", email=" + email +
-                '}';
-    }
 }
