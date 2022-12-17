9f2ed02b6de4647e77100659e5b4683c861b7b65
Dmitry Kharin <104873585+rotoryels290788@users.noreply.github.com>
2022-12-17 13:30:14 +0300

    Update UserDao.java

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -1,10 +1,7 @@
 package web.dao;
-import org.springframework.stereotype.Component;
 import web.model.User;
 import java.util.List;
 
-
-@Component
 public interface UserDao {
     List<User> getUserAll();
 
